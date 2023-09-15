beab171912f7b28df16ba3b4b5f9d4583a623f5e
anhndt.2000 <tuananh010320@gmail.com>
2023-09-15 17:55:05 +0700

    update code

--- flask_preview/index.py
+++ flask_preview/index.py
@@ -8,16 +8,39 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/api/data/nhanvien', methods=['GET'])
+@app.route('/api/data/hocsinh', methods=['GET'])
 def api_data():
     clear = lambda: os.system('cls')
     clear()
-    df = mysql_connector.get_nhanvien()
+    df = mysql_connector.get_hocsinh()
+    json_data = df.to_json(orient='records')
+    return Response(json_data, mimetype='application/json')
+
+@app.route('/api/data/create/table', methods=['Post']) 
+def api_create():
+    table_name = request.form.get('table_name')   
+    mysql_connector.create_table(table_name)
+    return ("Ok")
+
+@app.route('/api/data/create/inserts_table_test', methods=['Post']) 
+def api_create_table_test():
+    table_name = request.form.get('table_name')   
+    value_1 = request.form.get('values_1')   
+    value_2 = request.form.get('values_2')
+    values = [value_1,value_2]
+    mysql_connector.inserts_table_test(table_name,values)
+    return ("Ok")
+
+@app.route('/api/data/table_test', methods=['GET'])
+def api_data_table_test():
+    clear = lambda: os.system('cls')
+    clear()
+    df = mysql_connector.get_table_test()
     json_data = df.to_json(orient='records')
 
     # Return the JSON data as the response
     return Response(json_data, mimetype='application/json')
-    
+
 if __name__ == '__main__':
     app.run(debug=True)
 
--- flask_preview/mysql_connector.py
+++ flask_preview/mysql_connector.py
@@ -13,7 +13,7 @@ from flask import Flask, jsonify, request
 
 # Connection parameters
 host = '127.0.0.1'
-database = 'shopdb'
+database = 'dbschool_v1'
 user = 'root'
 password = '12345678'
 
@@ -24,9 +24,9 @@ cursor = conn.cursor()
 
 
 
-def get_nhanvien():
+def get_hocsinh():
         # Khởi tạo câu truy vấn với bảng món ăn bằng con trỏ (cursor)
-    cursor.execute("SELECT * FROM nhanvien")
+    cursor.execute("SELECT * FROM hocsinh")
 
     # Lấy tất cả các hàng từ câu truy vấn
     rows = cursor.fetchall()
@@ -35,11 +35,45 @@ def get_nhanvien():
     column_names = [column[0] for column in cursor.description]
 
     # Tạo dataframe df_sql_dishes từ "rows" và "column_names"
-    df_sql_nhanvien = pd.DataFrame(rows, columns=column_names)
+    df_sql_hocsinh = pd.DataFrame(rows, columns=column_names)
 
     # Tạo 1 dataframe "df_dishes" là bản sao của "df_sql_dishes"
     # df_dishes 
-    # json_data = jsonify(df_sql_nhanvien)
+    # json_data = jsonify(df_sql_hocsinh)
     # return json_data
-    return df_sql_nhanvien
-print(get_nhanvien())
+    return df_sql_hocsinh
+
+def create_table(table_name):
+    cursor.execute('''
+    CREATE TABLE Table_test (
+    Table_test_col_1  INT AUTO_INCREMENT PRIMARY KEY,
+    Table_test_col_2  VARCHAR(255) NOT NULL,
+    Table_test_col_3  TEXT );
+''')
+    
+def inserts_table_test(table_name, values):
+    
+    cursor.execute('''
+    INSERT INTO Table_test (Table_test_col_2, Table_test_col_3) 
+    VALUES
+    ('{0}', '{1}');
+'''.format(values[0],values[1]))
+    
+def get_table_test():
+        # Khởi tạo câu truy vấn với bảng món ăn bằng con trỏ (cursor)
+    cursor.execute("SELECT * FROM table_test")
+
+    # Lấy tất cả các hàng từ câu truy vấn
+    rows = cursor.fetchall()
+
+    # Lấy tất cả các cột/trường thuộc dữ liệu đã lấy từ câu truy vấn
+    column_names = [column[0] for column in cursor.description]
+
+    # Tạo dataframe df_sql_dishes từ "rows" và "column_names"
+    df_sql_table_test = pd.DataFrame(rows, columns=column_names)
+
+    # Tạo 1 dataframe "df_dishes" là bản sao của "df_sql_dishes"
+    # df_dishes 
+    # json_data = jsonify(df_sql_table_test)
+    # return json_data
+    return df_sql_table_test    
